582f8351c2cdab6be163a346a1df0cbf47a1b8fa
20sstanfield <20sstanfield@gmail.com>
2020-10-01 03:06:15 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int total = 0;
+    int car = 0;
+    for(int counter=0;counter<7;counter++)
+    {
+      car++;
+      Console.WriteLine("Input the distance for car#" + car);
+      double distance = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Input the time of car#" + car);
+      double time = Convert.ToDouble(Console.ReadLine());
+      double speed = distance/time;
+      Console.WriteLine("the speed of car#" + car + " is " + speed);
+      total++;
+
+    }
+    double average = total/car;
+    Console.WriteLine("the average of the cars is " + average);
+  }
+}
